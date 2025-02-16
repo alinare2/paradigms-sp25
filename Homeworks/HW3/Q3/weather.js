51047c81a5a2deb1517cd3095b267e2e2de13485
Andrew Linares <andrewlinaresch@gmail.com>
2025-02-16 17:45:33 -0500

    HW3: Q3 solution attempt 1

--- Homeworks/HW3/Q3/weather.js
+++ Homeworks/HW3/Q3/weather.js
@@ -2,11 +2,23 @@ function toCelsius(){
 
 	// grabs the input from the user
 	let input =  document.getElementById("temperature").value;
+
 	// converts the temperature to C
 	let celsius = (input - 32) * 5/9;
 
-	// show back to the user, on the <span> element
-	document.getElementById("result").innerText = celsius;
+	//check if input was number
+	if (isNaN(celsius)){
+		warning = "<div class=myparagraph>This is not a number!<div>";
+		let parent = document.getElementById("result-parent")
+		parent.innerHTML = warning;
+	}
+	else {
+		//restore parent
+		let parent = document.getElementById("result-parent");
+		parent.innerHTML = ` The Temperature in Celsius is <span id="result"></span>`;
+		// show back to the user, on the <span> element
+		document.getElementById("result").innerText = celsius;
+	}
 	// makes the div visible
 	// element.style can be used to change CSS properties of an HTML document
 	document.getElementById("result-parent").style.visibility = "visible";
